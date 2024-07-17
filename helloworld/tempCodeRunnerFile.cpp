54821bc89b46a3c7c3c2e4fbef6e4e68cc654443
woohyuk <7dngur7@naver.com>
2024-07-17 16:03:24 +0900

    7/17

--- helloworld/tempCodeRunnerFile.cpp
+++ helloworld/tempCodeRunnerFile.cpp
@@ -1,8 +1,1 @@
-        // for(int j=1; j<=n; j++){
-        //             for(int k=1; k<=n; k++){
-
-        //     cout<<  chkarr[j][k]<<" ";
-        //             }
-        //             cout<<"\n";
-
-        // }
+)
