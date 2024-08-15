1554f992425b9673b9e734f5e9524bae861e4dd6
woohyuk <7dngur7@naver.com>
2024-08-16 00:39:26 +0900

    8/16

--- helloworld/1005.cpp
+++ helloworld/1005.cpp
@@ -1,35 +1,65 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-vector< vector<int> > v(100000);
+vector< vector<int> > v(100005);
 
 int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     int t;
-    cin>>t;
+    cin >> t;
 
-    for(int i=0; i<t; i++){
-        int arr[101]={};
+    while(t--) {
+        int costarr[1002] = {};
+        int priorarr[1001] = {};
+        int dp[1001] = {};
         int n, k;
-        cin>>n>>k;
+        cin >> n >> k;
 
-        for(int j=0; j<n; j++){
-            cin>>arr[j];
+        for(int j = 1; j <= n; j++) {
+            cin >> costarr[j];
         }
-        for(int j=0; j<k;j++){
-
-
+        for(int j = 0; j < k; j++) {
+            int a, b;
+            cin >> a >> b;
+            priorarr[b]++;
+            v[a].push_back(b);
         }
 
+        int w;
+        cin >> w;
 
+        queue<int> q;
+        for(int j = 1; j <= n; j++) {
+            if(priorarr[j] == 0) {
+                q.push(j);
+                dp[j] = costarr[j];  // 초기 비용 설정
+            }
+        }
 
+        while(!q.empty()) {
+            int curr = q.front();
+            q.pop();
 
+            for(int next : v[curr]) {
+                dp[next] = max(dp[next], dp[curr] + costarr[next]);
+                priorarr[next]--;
+                if(priorarr[next] == 0) {
+                    q.push(next);
+                }
+            }
+        }
 
+        cout << dp[w] << "\n";
 
+        for(int j = 1; j <= n; j++) {
+            v[j].clear();
+        }
     }
 
+    return 0;
 }
