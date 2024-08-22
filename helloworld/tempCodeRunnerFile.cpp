aed97426a502b97db3cdaba50dad10a20683e4e3
woohyuk <7dngur7@naver.com>
2024-08-22 14:25:05 +0900

    8/21

--- helloworld/tempCodeRunnerFile.cpp
+++ helloworld/tempCodeRunnerFile.cpp
@@ -1,9 +1,68 @@
-    // cout<<arr[a.size()][b.size()]<<"\n";
-    // for(int i=1; i<=a.size(); i++){
-    //     for(int j=1; j<=b.size(); j++){
+#include <iostream>
+#include <queue>
 
-    //             cout<<arr[i][j];  
-    //     }
-    //     cout<<"\n";
+using namespace std;
 
-    // }
+int arr[1002][1002]={};
+queue< pair<int, int> >q;
+int xarr[4]={-1,0,1,0};
+int yarr[4]={0,1,0,-1};
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, m;
+    cin>>n>>m;
+
+    for(int i=0; i<n; i++){
+        string a;
+        cin>>a;
+        for(int j=0; j<m; j++){
+            arr[i][j]=a[j]-'0';
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(arr[i][j]>0){
+                int chkarr[1002][1002]={};
+                int curans=1;
+
+                q.push(make_pair(i,j));
+                chkarr[i][j]=1;
+
+                while(!q.empty()){
+                    pair<int,int> cur = q.front();
+                    q.pop();
+
+                    for(int i=0; i<4; i++){
+                        int xcur=cur.first+xarr[i];
+                        int ycur=cur.second+yarr[i];
+
+                        if(xcur<0||xcur>=n||ycur<0|| ycur>=m)continue;
+                        if(chkarr[xcur][ycur]>0)continue;
+                        if(arr[xcur][ycur]>0)continue;
+
+                        q.push(make_pair(xcur, ycur));
+                        chkarr[xcur][ycur]=1;
+                        curans++;
+
+                        //cout<<"cur:"<<xcur<<" "<<ycur<<"\n";
+                    }
+                }
+
+                arr[i][j]=curans%10;
+
+            }
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cout<<arr[i][j];
+        }
+        cout<<"\n";
+    }
+
+}
